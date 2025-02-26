e50f1826b73d494c4613d02434d8c4a1390077a7
LordeP26 <ped.li2608@gmail.com>
2025-02-26 13:33:32 -0300

    Getting a user by id

--- src/main/java/com/pedrolima/workshopmongo/services/UserService.java
+++ src/main/java/com/pedrolima/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.pedrolima.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedrolima.workshopmongo.domain.User;
 import com.pedrolima.workshopmongo.repository.UserRepository;
+import com.pedrolima.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
